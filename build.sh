82dea31721427a416fe291b6f155272e5ed99a0f
Tuzki Zhang <rabbitzhang52@gmail.com>
2015-04-25 16:11:02 +0800

    modify scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 docker build -t rabbit52/boot2docker .
-docker run --rm rabbit52/boot2docker > boot2docker.iso
---
+++ make.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --rm rabbit52/boot2docker > boot2docker.iso
